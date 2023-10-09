0b8603ad9b8dbafb56cda60851b7bb9d54ede643
Abdulkadir KOCER <akocerweb@hotmail.com>
2023-10-09 11:45:30 +0300

    Add project files.

---
+++ uyg01/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using uyg01.Models;
+
+namespace uyg01.Controllers
+{
+
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository productRepository;
+
+        public ProductController(ProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var products = productRepository.GetList();
+
+            return View(products);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            var product = productRepository.GetById(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            productRepository.Remove(product.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ uyg01/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace uyg01.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public bool Status { get; set; }
+
+    }
+}
