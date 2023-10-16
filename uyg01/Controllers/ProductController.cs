6f81e2fed6c4444aa3c7f67d2d3a6b247cdd8ae5
Abdulkadir KOCER <akocerweb@hotmail.com>
2023-10-16 11:45:40 +0300

    2. Hafta

--- uyg01/Controllers/ProductController.cs
+++ uyg01/Controllers/ProductController.cs
@@ -20,13 +20,34 @@ namespace uyg01.Controllers
             return View(products);
         }
 
-        public IActionResult Add()
+        public IActionResult Detail(int id)
+        {
+            var product = productRepository.GetById(id);
+            return View(product);
+        }
+
+        public IActionResult Insert()
         {
             return View();
         }
-        public IActionResult Edit()
+
+        [HttpPost]
+        public IActionResult Insert(Product product)
         {
-            return View();
+            productRepository.Add(product);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var product = productRepository.GetById(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            productRepository.Update(product);
+            return RedirectToAction("Index");
         }
         public IActionResult Delete(int id)
         {
--- uyg01/Models/Product.cs
+++ uyg01/Models/Product.cs
@@ -1,9 +1,21 @@
-﻿namespace uyg01.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace uyg01.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "Ürün Adı Giriniz")]
+
         public string Name { get; set; }
+
+
+
+        [Required(ErrorMessage = "Ürün Açıklaması Giriniz")]
         public string Description { get; set; }
 
         public bool Status { get; set; }
